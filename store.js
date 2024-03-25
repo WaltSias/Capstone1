518e77bf40d0d0afd1a76c1619e74f748c9c392b
WaltSias <walt.sias@gmail.com>
2024-03-24 20:19:50 -0500

    added event listener to store.js

---
+++ store.js
@@ -0,0 +1,13 @@
+
+document.getElementById("price1").addEventListener("click", function () {
+    alert("Sold OUT!");
+    this.style.width = "80px";
+});
+document.getElementById("price2").addEventListener("click", function () {
+    alert("Sold OUT!");
+    this.style.width = "80px";
+});
+document.getElementById("price3").addEventListener("click", function () {
+    alert("Sold OUT!");
+    this.style.width = "80px";
+});
